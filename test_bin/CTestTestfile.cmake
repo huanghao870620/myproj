5c4d3aa0dd3169fd41838ba9e6899e74f41a3203
huanghao870620 <huanghao870620@hotmail.com>
2016-03-01 15:04:36 +0800

    deleted:    test_bin/CMakeFiles/generate.stamp 	deleted:    test_bin/CMakeFiles/generate.stamp.depend 	deleted:    test_bin/CTestTestfile.cmake 	deleted:    test_bin/PACKAGE.vcxproj 	deleted:    test_bin/PACKAGE.vcxproj.filters 	deleted:    test_bin/cmake_install.cmake

--- test_bin/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/gitup/myproj/test
-# Build directory: D:/gitup/myproj/test_bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(name "your_test" "command" "your_test")
