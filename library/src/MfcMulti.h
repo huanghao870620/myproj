00ffd5fbff01e2c7f05a173062cfd9201db19f0e
huanghao870620 <huanghao870620@hotmail.com>
2016-03-01 20:31:29 +0800

    modified:   library/src/MfcMulti.h

--- library/src/MfcMulti.h
+++ library/src/MfcMulti.h
@@ -1,34 +1,28 @@
 #ifndef MFCMULTI_H
 #define MFCMULTI_H
+
 #include <list>
+
 class Client;
 class CWinThread;
 class RecvThread;
 class StruInte;
 class DrawFlag;
 
-
 class MfcMulti
 {
-
-
     public:
-
         MfcMulti(); 
-		 
         ~MfcMulti(); 
-
 		void Start();
-
 		std::list<StruInte *> * get_st();
-
 		Client & getClientRef();
+
 	private:
 		Client * client;
 		RecvThread *recvThread = nullptr;
 		std::list<StruInte *> st;
 		CWinThread *pThread = nullptr;
-
 };  
 
 
