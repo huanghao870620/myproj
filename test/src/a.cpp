df135689de51ed24b54927b14ac0b7507116f30c
hao.huang <1175210752@qq.com>
2016-06-15 19:54:17 +0800

    commit

--- test/src/a.cpp
+++ test/src/a.cpp
@@ -2,12 +2,34 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/parameterized_test.hpp>
 
-
-BOOST_AUTO_TEST_SUITE(stringtest)
+using boost::unit_test::test_suite;
+test_suite* init_unit_test_suite(int, char*[]){
+	std::cout << "222" << std::endl;
+	return 0;
+}
+BOOST_AUTO_TEST_SUITE(a)
+/*
 
 BOOST_AUTO_TEST_CASE(test0)
 {
 	std::cout << "hello" << std::endl;
+	
 }
 
+using boost::unit_test::test_suite;
+
+test_suite* init_unit_test_suite(int, char*[])
+{
+	//framework::master_test_suite().p_name.value = "Unit test example 01";
+	// register the test case in test tree and specify number of expected failures so   
+	// this example will pass at runtime. We expect 2 errors: one from failed check and    
+	// one from memory acces violation   
+	//framework::master_test_suite().add(BOOST_TEST_CASE(&myTest), 1);
+	
+	return 0;
+}
+
+
+*/
 BOOST_AUTO_TEST_SUITE_END()
+
