598bd91864d01262d997c7d2ce044a9d53b70301
hao.huang <1175210752@qq.com>
2016-07-08 18:09:48 +0800

    commit

---
+++ test/src/MyTest.cpp
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////
+//  MyTest.cpp
+//  Implementation of the Class MyTest
+//  Created on:      08-ÆßÔÂ-2016 17:58:34
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+#include<iostream>
+#include "MyTest.h"
+
+
+MyTest::MyTest(){
+
+}
+
+
+
+MyTest::~MyTest(){
+
+}
+
+void MyTest::test0(int a[]){
+	std::cout << "" << std::endl;
+}
---
+++ test/src/MyTest.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////
+//  MyTest.h
+//  Implementation of the Class MyTest
+//  Created on:      08-ÆßÔÂ-2016 17:58:34
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_77122CA7_9414_4921_8809_99FE786EDE83__INCLUDED_)
+#define EA_77122CA7_9414_4921_8809_99FE786EDE83__INCLUDED_
+
+class MyTest
+{
+
+public:
+	MyTest();
+	virtual ~MyTest();
+
+	void test0(int a[]);
+};
+#endif // !defined(EA_77122CA7_9414_4921_8809_99FE786EDE83__INCLUDED_)
