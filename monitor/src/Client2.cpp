139e8dc7aa01125fd8614477d397c60cb4e90f68
hao.huang <1175210752@qq.com>
2016-07-20 16:41:35 +0800

    commit

---
+++ monitor/src/Client2.cpp
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////
+//  Client.cpp
+//  Implementation of the Class Client
+//  Created on:      20-七月-2016 13:37:45
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+#include "ReadIniFile.h"
+#include "Client2.h"
+
+#define DEFAULT_BUFLEN 512
+
+Client2::Client2(){
+	WSADATA wsaData;
+	WORD versionRequested = MAKEWORD(1, 1);
+	WSAStartup(versionRequested, &wsaData); //初始化dll
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 创建socket
+	if (sock <= 0){
+		::MessageBox(NULL, _T("创建socket失败"), _T(""), MB_OK);
+		return;
+	}
+
+	SOCKADDR_IN sockaddr_in;
+	sockaddr_in.sin_family = AF_INET;
+	sockaddr_in.sin_port = htons(6000);
+
+	char server_ip[100];
+	ReadIniFile::get_server_ip(server_ip);
+	sockaddr_in.sin_addr.S_un.S_addr = inet_addr(server_ip);
+	int resp = connect(sock, (SOCKADDR*)& sockaddr_in, sizeof(sockaddr_in));
+	if (resp != 0){
+		::MessageBox(NULL, _T("socket连接失败"), _T(""), MB_OK);
+		return;
+	}
+
+}
+
+
+
+Client2::~Client2(){
+
+}
---
+++ monitor/src/Client2.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  Client.h
+//  Implementation of the Class Client
+//  Created on:      20-ÆßÔÂ-2016 13:37:45
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_ECDE219A_7BEB_4eb0_A208_BF8E8F1DC903__INCLUDED_)
+#define EA_ECDE219A_7BEB_4eb0_A208_BF8E8F1DC903__INCLUDED_
+#include<afxwin.h>
+/**
+ * ¿Í»§¶Ë
+ */
+class Client2
+{
+
+public:
+	Client2();
+	virtual ~Client2();
+
+};
+#endif // !defined(EA_ECDE219A_7BEB_4eb0_A208_BF8E8F1DC903__INCLUDED_)
