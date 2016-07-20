80dab075df34d50392f9e448e70db7ff5261d4d9
hao.huang <1175210752@qq.com>
2016-07-20 10:42:26 +0800

    Signed-off-by: hao.huang <1175210752@qq.com>

---
+++ test/src/ReceiverTest.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  ReceiverTest.cpp
+//  Implementation of the Class ReceiverTest
+//  Created on:      20-ÆßÔÂ-2016 10:28:55
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+#include "Receiver.h"
+#include "ReceiverTest.h"
+
+ReceiverTest::ReceiverTest(){
+
+}
+
+
+
+ReceiverTest::~ReceiverTest(){
+
+}
+
+void ReceiverTest::testConstructor(){
+
+}
---
+++ test/src/ReceiverTest.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+//  ReceiverTest.h
+//  Implementation of the Class ReceiverTest
+//  Created on:      20-ÆßÔÂ-2016 10:28:55
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_7350E9A6_396A_4ec0_BCEF_EE94C5B6AC3B__INCLUDED_)
+#define EA_7350E9A6_396A_4ec0_BCEF_EE94C5B6AC3B__INCLUDED_
+
+class ReceiverTest
+{
+
+public:
+	ReceiverTest();
+	virtual ~ReceiverTest();
+
+	void testConstructor();
+
+};
+#endif // !defined(EA_7350E9A6_396A_4ec0_BCEF_EE94C5B6AC3B__INCLUDED_)
