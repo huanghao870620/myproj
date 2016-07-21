a3a21113efc71b0dd330ece5f9b53209d913ce41
hao.huang <1175210752@qq.com>
2016-07-21 10:37:16 +0800

    commit

---
+++ chat_server/Dto.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  Dto.cpp
+//  Implementation of the Class Dto
+//  Created on:      21-ÆßÔÂ-2016 10:28:51
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "Dto.h"
+
+
+Dto::Dto(){
+
+}
+
+
+
+Dto::~Dto(){
+
+}
---
+++ chat_server/Dto.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  Dto.h
+//  Implementation of the Class Dto
+//  Created on:      21-七月-2016 10:28:51
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_C550C720_F69C_4b2a_8F4F_65F7ECF8CF36__INCLUDED_)
+#define EA_C550C720_F69C_4b2a_8F4F_65F7ECF8CF36__INCLUDED_
+#include<list>
+#include<afxwin.h>
+class Dto
+{
+
+public:
+	Dto();
+	virtual ~Dto();
+
+private:
+	std::list<SOCKET *> clients; // 所有客户端
+};
+#endif // !defined(EA_C550C720_F69C_4b2a_8F4F_65F7ECF8CF36__INCLUDED_)
