92024ea732b22ae731183fd6b839184d89e2e998
hao.huang <1175210752@qq.com>
2016-07-25 15:03:20 +0800

    commit

---
+++ test/src/Client2Test.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  Client2Test.cpp
+//  Implementation of the Class Client2Test
+//  Created on:      25-ÆßÔÂ-2016 14:58:50
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+#include "Client2Test.h"
+
+
+Client2Test::Client2Test(){
+
+}
+
+
+
+Client2Test::~Client2Test(){
+
+}
+
+void Client2Test::testClient2(){
+
+}
---
+++ test/src/Client2Test.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////
+//  Client2Test.h
+//  Implementation of the Class Client2Test
+//  Created on:      25-ÆßÔÂ-2016 14:58:50
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_370FC595_E572_47a4_A516_226B60677D1A__INCLUDED_)
+#define EA_370FC595_E572_47a4_A516_226B60677D1A__INCLUDED_
+
+class Client2Test
+{
+
+public:
+	Client2Test();
+	virtual ~Client2Test();
+
+	void testClient2();
+};
+#endif // !defined(EA_370FC595_E572_47a4_A516_226B60677D1A__INCLUDED_)
