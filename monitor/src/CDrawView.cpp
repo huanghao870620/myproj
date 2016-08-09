f96a612a51a480930bfb0a9c2134245726c763be
hao.huang <1175210752@qq.com>
2016-08-09 17:58:50 +0800

    commit

---
+++ monitor/src/CDrawView.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  CDrawView.cpp
+//  Implementation of the Class CDrawView
+//  Created on:      09-°ËÔÂ-2016 16:09:35
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "CDrawView.h"
+
+
+CDrawView::CDrawView(){
+
+}
+
+
+
+CDrawView::~CDrawView(){
+
+}
---
+++ monitor/src/CDrawView.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+//  CDrawView.h
+//  Implementation of the Class CDrawView
+//  Created on:      09-°ËÔÂ-2016 16:09:34
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_8AA7A857_155C_41cf_942E_C45CB3286775__INCLUDED_)
+#define EA_8AA7A857_155C_41cf_942E_C45CB3286775__INCLUDED_
+
+#include<afxwin.h>
+class CDrawView : public CScrollView
+{
+	DECLARE_DYNCREATE(CDrawView)
+
+public:
+	CDrawView();
+	virtual ~CDrawView();
+
+};
+#endif // !defined(EA_8AA7A857_155C_41cf_942E_C45CB3286775__INCLUDED_)
