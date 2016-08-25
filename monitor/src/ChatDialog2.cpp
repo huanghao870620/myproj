aca57d4a75cd7a00afeec8ccc8d151b645ef127f
hao.huang <1175210752@qq.com>
2016-08-25 14:45:46 +0800

    commit

--- monitor/src/ChatDialog2.cpp
+++ monitor/src/ChatDialog2.cpp
@@ -20,7 +20,8 @@ ChatDialog2::~ChatDialog2(){
 }
 
 BOOL ChatDialog2::OnInitDialog(){
-	/*CDialog::OnInitDialog();
+	/*
+	CDialog::OnInitDialog();
 	UINT targetCtrlId = IDC_STATIC;
 	CWnd *pWnd = this->GetDlgItem(targetCtrlId);
 	CRect rectTargetCtrl;
@@ -34,7 +35,9 @@ BOOL ChatDialog2::OnInitDialog(){
 	this->m_pDrawView = (CDrawView*)RUNTIME_CLASS(CDrawView)->CreateObject();
 	this->m_pDrawView->Create(NULL, NULL, AFX_WS_DEFAULT_VIEW | WS_VSCROLL | WS_HSCROLL, rectTargetCtrl, this, targetCtrlId);*/
 
-
+	CDialog::OnInitDialog();
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0XF000);
 
 
 	return TRUE;
