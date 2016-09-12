92e454e27b796763eb246e7162b67fddd8ca03d3
hao.huang <1175210752@qq.com>
2016-09-12 18:00:09 +0800

    commit

--- example2/CScrollDemoView.h
+++ example2/CScrollDemoView.h
@@ -7,6 +7,7 @@
 
 #if !defined(EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_)
 #define EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_
+#include"MyDoc.h"
 #include<afxwin.h>
 class CScrollDemoView : public CScrollView
 {
@@ -15,5 +16,21 @@ public:
 	CScrollDemoView();
 	virtual ~CScrollDemoView();
 	DECLARE_DYNCREATE(CScrollDemoView)
+
+public: MyDoc* GetDocument();
+public:virtual void OnDraw(CDC *pDC);
+public:virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:virtual void OnInitialUpdate();
+protected:BOOL m_bSmooth;
+protected:void GetCellRect(int row, int col, LPRECT pRect);
+protected:void DrawAddress(CDC *pDC, int row, int col);
+protected:void DrawPointer(CDC *pDc, int row, int col, BOOL bHighlight);
+protected: CFont m_font;
+protected:int m_nCurrentCol;
+protected:int m_nCurrentRow;
+protected:int m_nRibbonWidth;
+protected:int m_nCellHeight;
+protected:int m_nCellWidth;
+protected: DECLARE_MESSAGE_MAP()
 };
 #endif // !defined(EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_)
