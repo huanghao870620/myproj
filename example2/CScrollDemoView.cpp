973789313299550a844e69dfea2c28294ea32dce
hao.huang <1175210752@qq.com>
2016-09-12 17:39:14 +0800

    commit

---
+++ example2/CScrollDemoView.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  CScrollDemoView.cpp
+//  Implementation of the Class CScrollDemoView
+//  Created on:      12-¾ÅÔÂ-2016 17:34:53
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "CScrollDemoView.h"
+
+
+CScrollDemoView::CScrollDemoView(){
+
+}
+
+
+
+CScrollDemoView::~CScrollDemoView(){
+
+}
---
+++ example2/CScrollDemoView.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  CScrollDemoView.h
+//  Implementation of the Class CScrollDemoView
+//  Created on:      12-¾ÅÔÂ-2016 17:34:53
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_)
+#define EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_
+#include<afxwin.h>
+class CScrollDemoView : public CScrollView
+{
+
+public:
+	CScrollDemoView();
+	virtual ~CScrollDemoView();
+	DECLARE_DYNCREATE(CScrollDemoView)
+};
+#endif // !defined(EA_CA6B56CD_31C4_495b_AFE8_73426F02D3B7__INCLUDED_)
