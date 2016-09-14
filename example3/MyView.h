5285265cb6141e73e9303b963b6d38ff680d7ab4
hao.huang <1175210752@qq.com>
2016-09-14 14:27:59 +0800

    commit

--- example3/MyView.h
+++ example3/MyView.h
@@ -7,13 +7,26 @@
 
 #if !defined(EA_753C7D69_F941_4600_A5DD_C99DDFFB08D9__INCLUDED_)
 #define EA_753C7D69_F941_4600_A5DD_C99DDFFB08D9__INCLUDED_
+#include"MyDoc.h"
 #include<afxwin.h>
-class MyView
+#include<afxhtml.h>
+class MyView : public CHtmlView
 {
-
+	DECLARE_DYNCREATE(MyView,CHtmlView)
 public:
 	MyView();
 	virtual ~MyView();
+	DECLARE_MESSAGE_MAP()
 
+public:MyDoc* GetDocument();
+public:virtual void OnDraw(CDC *pDC);
+public:virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+public:virtual void OnTitleChange(LPCTSTR lpszText);
+protected: virtual void OnInitialUpdate();
+
+#ifdef _DEBUG
+public:virtual void AssertValid() const;
+public:virtual void Dump(CDumpContext&dc) const;
+#endif
 };
 #endif // !defined(EA_753C7D69_F941_4600_A5DD_C99DDFFB08D9__INCLUDED_)
