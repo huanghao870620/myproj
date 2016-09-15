28f18debf9355ad6f61a3554abc563be2f01d209
hao.huang <1175210752@qq.com>
2016-09-15 15:17:31 +0800

    commit

---
+++ example4/MyView.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  MyView.h
+//  Implementation of the Class MyView
+//  Created on:      15-¾ÅÔÂ-2016 15:16:02
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_4CDCA07D_9021_4184_A748_CF557CCF8606__INCLUDED_)
+#define EA_4CDCA07D_9021_4184_A748_CF557CCF8606__INCLUDED_
+
+class MyView
+{
+
+public:
+	MyView();
+	virtual ~MyView();
+
+};
+#endif // !defined(EA_4CDCA07D_9021_4184_A748_CF557CCF8606__INCLUDED_)
