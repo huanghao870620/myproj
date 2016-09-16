e2ff647e27ac7fe4feec5edd0611895e47cfdb08
hao.huang <1175210752@qq.com>
2016-09-16 16:52:50 +0800

    commit

--- example7/MyView.cpp
+++ example7/MyView.cpp
@@ -9,6 +9,7 @@
 
 IMPLEMENT_DYNCREATE(MyView,CView)
 BEGIN_MESSAGE_MAP(MyView,CView)
+	ON_WM_CREATE()
 END_MESSAGE_MAP()
 MyView::MyView(){
 	
@@ -27,5 +28,6 @@ void MyView::OnDraw(CDC*pDC){
 BOOL MyView::OnCreate(LPCREATESTRUCT lpCreateStruct){
 	CRect rect(0, 0, 100, 100);
 	this->m_wndPushButton.Create("ddd", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, rect, this, IDC_BUTTON);
+	this->m_wndPushButton.ShowWindow(SW_SHOW);
 	return TRUE;
 }
