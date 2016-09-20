41f519382bac119d0eab0275dc1f058cf1249857
hao.huang <1175210752@qq.com>
2016-09-20 10:09:55 +0800

    commit

--- example10/MyApp.cpp
+++ example10/MyApp.cpp
@@ -19,5 +19,22 @@ MyApp::~MyApp(){
 }
 
 BOOL MyApp::InitInstance(){
+	AllocConsole();
+	SetConsoleTitle("");
+	freopen("CONIN$", "r", stdin);
+	freopen("CONOUT$", "w+t", stdout);
+
+	CSingleDocTemplate *temp;
+	temp = new CSingleDocTemplate(IDR_MAINFRAME, RUNTIME_CLASS(MyDoc), RUNTIME_CLASS(MyFrame), RUNTIME_CLASS(MyView));
+	AddDocTemplate(temp);
+
+	CCommandLineInfo info;
+	ParseCommandLine(info);
+	if (!ProcessShellCommand(info)){
+		return FALSE;
+	}
+
+	this->m_pMainWnd->ShowWindow(SW_SHOW);
+	this->m_pMainWnd->UpdateWindow();
 	return TRUE;
 }
--- example10/MyApp.h
+++ example10/MyApp.h
@@ -7,6 +7,10 @@
 
 #if !defined(EA_0D0730EA_6CBB_49ff_9B7F_690B9F328F68__INCLUDED_)
 #define EA_0D0730EA_6CBB_49ff_9B7F_690B9F328F68__INCLUDED_
+#include"resource.h"
+#include"MyDoc.h"
+#include"MyFrame.h"
+#include"MyView.h"
 #include<afxwin.h>
 class MyApp : public CWinApp
 {
