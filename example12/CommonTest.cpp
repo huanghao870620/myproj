4f20795e681aa3d119e49f4df2bfdcef3b9fa2c9
hao.huang <1175210752@qq.com>
2016-09-21 09:56:26 +0800

    commit

---
+++ example12/CommonTest.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  CommonTest.cpp
+//  Implementation of the Class CommonTest
+//  Created on:      21-¾ÅÔÂ-2016 9:54:46
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "CommonTest.h"
+
+
+CommonTest::CommonTest(){
+
+}
+
+
+
+CommonTest::~CommonTest(){
+
+}
---
+++ example12/CommonTest.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  CommonTest.h
+//  Implementation of the Class CommonTest
+//  Created on:      21-¾ÅÔÂ-2016 9:54:46
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_CAD8D753_24BB_4f20_A6A9_BA64CF5ABDA3__INCLUDED_)
+#define EA_CAD8D753_24BB_4f20_A6A9_BA64CF5ABDA3__INCLUDED_
+
+/**
+ * test
+ */
+class CommonTest
+{
+
+public:
+	CommonTest();
+	virtual ~CommonTest();
+
+};
+#endif // !defined(EA_CAD8D753_24BB_4f20_A6A9_BA64CF5ABDA3__INCLUDED_)
