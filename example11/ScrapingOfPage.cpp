b77a75a6e3ba4abc59ecd2eb00cd0f092390fd76
hao.huang <1175210752@qq.com>
2016-09-21 09:49:23 +0800

    commit

---
+++ example11/ScrapingOfPage.cpp
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//  ScrapingOfPage.cpp
+//  Implementation of the Class ScrapingOfPage
+//  Created on:      21-¾ÅÔÂ-2016 9:47:37
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "ScrapingOfPage.h"
+
+
+ScrapingOfPage::ScrapingOfPage(){
+
+}
+
+
+
+ScrapingOfPage::~ScrapingOfPage(){
+
+}
---
+++ example11/ScrapingOfPage.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////
+//  ScrapingOfPage.h
+//  Implementation of the Class ScrapingOfPage
+//  Created on:      21-¾ÅÔÂ-2016 9:47:37
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_37605C5E_A1FA_4def_914F_AB882473BCC3__INCLUDED_)
+#define EA_37605C5E_A1FA_4def_914F_AB882473BCC3__INCLUDED_
+#include<iostream>
+/**
+ * ×¥È¡ÍøÒ³
+ */
+class ScrapingOfPage
+{
+
+public:
+	ScrapingOfPage();
+	virtual ~ScrapingOfPage();
+
+};
+#endif // !defined(EA_37605C5E_A1FA_4def_914F_AB882473BCC3__INCLUDED_)
