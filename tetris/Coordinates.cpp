4e66359e394006399a3d9d954ba61b1123f860a7
hao.huang <1175210752@qq.com>
2016-09-26 19:28:14 +0800

    commit

---
+++ tetris/Coordinates.cpp
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////
+//  Coordinates.cpp
+//  Implementation of the Class Coordinates
+//  Created on:      26-¾ÅÔÂ-2016 16:23:48
+//  Original author: huang.hao
+///////////////////////////////////////////////////////////
+
+#include "Coordinates.h"
+
+
+//Coordinates::Coordinates(int x,int y){
+//	this->x = x;
+//	this->y = y;
+//}
+
+
+Coordinates::Coordinates(){
+
+}
+
+
+Coordinates::~Coordinates(){
+
+}
+//
+//int Coordinates::getX(){
+//	return this->x;
+//}
+//
+//int Coordinates::getY(){
+//	return this->y;
+//}
+//
+//void Coordinates::setX(int x){
+//	this->x = x;
+//}
+//
+//void Coordinates::setY(int y){
+//	this->y = y;
+//}
