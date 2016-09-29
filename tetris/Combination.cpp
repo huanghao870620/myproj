5a904c82ac723ed8a6dbffc30bd54929a6d389bd
hao.huang-home <huanghao870620@163.com>
2016-09-29 18:11:25 +0800

    commit

--- tetris/Combination.cpp
+++ tetris/Combination.cpp
@@ -11,12 +11,49 @@
 Combination::Combination(CDC*pDC,CBrush *brush){
 	this->pDC = pDC;
 	this->brush = brush;
+
+	Singleton *inst = Singleton::getSingleton();
+	std::list<Coordinates*>::iterator iter2 = inst->getCoors()->begin();
+	Coordinates *coor0 = *iter2;
+	int x = coor0->getX();
+	int y = coor0->getY();
+
+	rect2 = new CRect;
+	rect2->left = x;
+	rect2->top = y;
+
+	rect2->right = x + MyView::sideLength;
+	rect2->bottom = y + MyView::sideLength;
+
+
+	c0 = new CRect;
+	c0->left = rect2->left + MyView::sideLength + MyView::gap;
+	c0->top = rect2->top;
+	c0->bottom = rect2->bottom;
+	c0->right = rect2->right + MyView::sideLength + MyView::gap;
+
+	c1 = new CRect;
+	c1->left = c0->left + MyView::sideLength + MyView::gap;
+	c1->top = rect2->top;
+	c1->bottom = rect2->bottom;
+	c1->right = c0->right + MyView::sideLength + MyView::gap;
+
+	c2 = new CRect;
+	c2->left = c1->left + MyView::sideLength + MyView::gap;
+	c2->top = rect2->top;
+	c2->right = c1->right + MyView::sideLength + MyView::gap;
+	c2->bottom = rect2->bottom;
+
+	this->rectList.push_back(rect2);
+	this->rectList.push_back(c0);
+	this->rectList.push_back(c1);
+	this->rectList.push_back(c2);
 }
 
 
 
 Combination::~Combination(){
-
+	
 }
 
 
@@ -33,50 +70,21 @@ Combination* Combination::getOne(){
 }
 
 void Combination::DrawCom(){
-	Singleton *inst = Singleton::getSingleton();
-	std::list<Coordinates*>::iterator iter2 = inst->getCoors()->begin();
-	Coordinates *coor0 = *iter2;
-	int x = coor0->getX();
-	int y = coor0->getY();
-
-	rect2 = new CRect;
-	this->rectList.push_back(rect2);
-	rect2->left = x;
-	rect2->top = y;
-
-	
-	rect2->right = x + MyView::sideLength;
-	rect2->bottom = y + MyView::sideLength;
 	pDC->FillRect(rect2, brush);
-
-
-	c0 = new CRect;
-	this->rectList.push_back(c0);
-	c0->left = rect2->left + MyView::sideLength + MyView::gap;
-	c0->top = rect2->top;
-	c0->bottom = rect2->bottom;
-	c0->right = rect2->right + MyView::sideLength + MyView::gap;
 	pDC->FillRect(c0, brush);
-
-	c1 = new CRect;
-	this->rectList.push_back(c1);
-	c1->left = c0->left + MyView::sideLength + MyView::gap;
-	c1->top = rect2->top;
-	c1->bottom = rect2->bottom;
-	c1->right = c0->right + MyView::sideLength + MyView::gap;
 	pDC->FillRect(c1, brush);
-
-	c2 = new CRect;
-	this->rectList.push_back(c2);
-	c2->left = c1->left + MyView::sideLength + MyView::gap;
-	c2->top = rect2->top;
-	c2->right = c1->right + MyView::sideLength + MyView::gap;
-	c2->bottom = rect2->bottom;
 	pDC->FillRect(c2, brush);
-
-	//std::cout << "size : " << this->rectList.size() << std::endl;
+	this->landing();
 }
 
 
 void Combination::landing(){
+	std::list<CRect*>::iterator iter = this->rectList.begin();
+	while (iter != this->rectList.end())
+	{
+		CRect *rect = *iter;
+		rect->top = rect->top + MyView::sideLength + MyView::gap;
+		rect->bottom = rect->bottom + MyView::sideLength + MyView::gap;
+		iter++;
+	}
 }
