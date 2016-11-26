c8d4c85a7597e4d8b9d635ef1af9f2d089691c80
hao.huang-home <huanghao870620@163.com>
2016-11-26 23:17:05 +0800

    commit

--- example31/MyFrame.cpp
+++ example31/MyFrame.cpp
@@ -8,10 +8,9 @@
 #include "MyFrame.h"
 
 IMPLEMENT_DYNCREATE(MyFrame,CFrameWnd)
-BEGIN_MESSAGE_MAP(MyFrame,CFrameWnd)
-END_MESSAGE_MAP()
-MyFrame::MyFrame(){
 
+MyFrame::MyFrame(){
+	std::cout << "aas" << std::endl;
 }
 
 
@@ -19,3 +18,18 @@ MyFrame::MyFrame(){
 MyFrame::~MyFrame(){
 
 }
+
+int MyFrame::OnCreate(LPCREATESTRUCT lpCreateStruct){
+	//::DockPane
+	//CFrameWnd::OnCreate(lpCreateStruct);
+	/*if (!this->m_wndDlgDisplay.Create("跟踪目标信息显示", this, CRect(200, 100, 300, 300), TRUE, IDD_DIALOG_DISPLAY, WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN | CBRS_BOTTOM | CBRS_FLOAT_MULTI, AFX_CBRS_REGULAR_TABS, AFX_CBRS_REGULAR_TABS| AFX_CBRS_AUTOHIDE | AFX_CBRS_RESIZE)){
+		TRACE0("未能创建停靠窗口");
+		return FALSE;
+	}
+	this->m_wndDlgDisplay.EnableDocking(CBRS_ALIGN_ANY);*/
+	return 0;
+}
+
+BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)
+	ON_WM_CREATE()
+END_MESSAGE_MAP()
--- example31/MyFrame.h
+++ example31/MyFrame.h
@@ -8,12 +8,16 @@
 #if !defined(EA_58D9A09A_F716_414d_AAE6_0A6EB272648E__INCLUDED_)
 #define EA_58D9A09A_F716_414d_AAE6_0A6EB272648E__INCLUDED_
 #include<afxwin.h>
+#include"DlgDisplayPane.h"
+#include<iostream>
 class MyFrame : CFrameWnd
 {
 
 public:
 	MyFrame();
 	virtual ~MyFrame();
+private:CDlgDisplayPane m_wndDlgDisplay;
+protected:afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 protected:DECLARE_DYNCREATE(MyFrame,CFrameWnd)
 protected:DECLARE_MESSAGE_MAP(MyFrame,CFrameWnd)
 };
